13a70a3fb44df85e4aab0bb2f1a7fb1910db1ef3
SeanBurkeDesign <burke.sean@gmail.com>
2016-07-05 09:32:23 -0400

    refactor/remove unnecessary functions

--- js/main.js
+++ js/main.js
@@ -10,59 +10,54 @@ function main() {
 	// Fade in .navbar
 	$(function () {
 		$(window).scroll(function () {
-            // set distance user needs to scroll before we fadeIn navbar
+      // set distance user needs to scroll before we fadeIn navbar
 			if ($(this).scrollTop() > 50) {
 				$('.navbar').fadeIn();
 			} else {
 				$('.navbar').fadeOut();
 			}
 		});
-
-	
 	});
 	
-	// Preloader */
-	  	$(window).load(function() {
+  // Preloader */
+  $(window).load(function() {
+    // will first fade out the loading animation 
+    $("#status").fadeOut("slow"); 
+    // will fade out the whole DIV that covers the website. 
+    $("#preloader").delay(500).fadeOut("slow").remove();      
+  }) 
 
-   	// will first fade out the loading animation 
-    	$("#status").fadeOut("slow"); 
+ // Page scroll
+	$('a.page-scroll').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 40
+        }, 900);
+        return false;
+      }
+    }
+  });
 
-    	// will fade out the whole DIV that covers the website. 
-    	$("#preloader").delay(500).fadeOut("slow").remove();      
+  // Show Menu on Book
+  $(window).bind('scroll', function() {
+    var navHeight = $(window).height() - 100;
+    if ($(window).scrollTop() > navHeight) {
+      $('.navbar-default').addClass('on');
+    } else {
+      $('.navbar-default').removeClass('on');
+    }
+  });
 
-  	}) 
-
-   // Page scroll
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 40
-            }, 900);
-            return false;
-          }
-        }
-      });
-
-    // Show Menu on Book
-    $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 100;
-        if ($(window).scrollTop() > navHeight) {
-            $('.navbar-default').addClass('on');
-        } else {
-            $('.navbar-default').removeClass('on');
-        }
-    });
-
-    $('body').scrollspy({ 
-        target: '.navbar-default',
-        offset: 80
-    })
+  $('body').scrollspy({ 
+    target: '.navbar-default',
+    offset: 80
+  })
 
   	$(document).ready(function() {
-  	    $(".testimonial").owlCarousel({
+	    $(".testimonial").owlCarousel({
         navigation: false, // Show next and prev buttons
         pagination: false,
         autoPlay: true,
@@ -70,38 +65,8 @@ function main() {
         slideSpeed: 700,
         paginationSpeed: 400,
         singleItem: true
-        });
-
+      });
   	});
-
-  	// Portfolio Isotope Filter
-    $(window).load(function() {
-        var $container = $('.portfolio-items');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
-
-    });
-	
 
   // Parallax
   function initParallax() {
@@ -112,7 +77,7 @@ function main() {
   };
   initParallax();
 
-  	// Pretty Photo
+  // Pretty Photo
 	$("a[rel^='prettyPhoto']").prettyPhoto({
 		social_tools: false,
     theme: 'facebook',
